7bf921453f13f67380c43aeb9f111d3659a9e90b
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-09-28 17:18:20 -0300

    Se agrega configuración de handlebars y servidor socket

--- proyecto-backend/src/app.js
+++ proyecto-backend/src/app.js
@@ -1,23 +1,38 @@
 import express, { json, urlencoded } from 'express';
-import {__dirname} from './dirname.js';
 import path from "path";
+import {__dirname} from './dirname.js';
 import { productsRouter } from './routes/products.routes.js';
 import { cartRouter } from './routes/cart.routes.js';
+import {engine} from "express-handlebars";
+import {Server} from "socket.io";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //Dar acceso al usuario a la carpeta public
-app.use(express.static(path.join("public")));
+app.use(express.static(path.join( __dirname,"public")));
 
 const port = 8080;
 
-app.listen(port, () => {
+const httpServer =app.listen(port, () => {
   console.log('Servidor en línea!');
 });
 
+const socketServer = new Server(httpServer);
+
+//Configuración handlebars
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,'./views'));
+
+//routes
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
+
+//Conexión de socket
+socketServer.on("connection", (socket) => {
+  console.log("Cliente conectado", socket.id);
+});
 
 
