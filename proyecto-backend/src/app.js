addff37d97c5da345fbf36d5137c25741f2abb93
Francisco Bourquin <122411274+FranciscoBourquin@users.noreply.github.com>
2023-10-15 18:49:13 -0300

    intercambio de mensajes entre app y chat.js

--- proyecto-backend/src/app.js
+++ proyecto-backend/src/app.js
@@ -1,42 +1,60 @@
-import express, { json, urlencoded } from 'express';
-import path from "path";
-import {__dirname} from './dirname.js';
+import express from 'express';
+import path from 'path';
+import { __dirname } from './dirname.js';
+import { json, urlencoded } from 'express';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
+import { connectDB } from './config/dbConnection.js';
+import { chatModel } from './dao/models/chat.model.js';
 import { productsRouter } from './routes/products.routes.js';
 import { cartRouter } from './routes/cart.routes.js';
-import {engine} from "express-handlebars";
-import {Server} from "socket.io";
-import { connectDB } from './config/dbConnection.js';
+import { chatRouter } from './routes/chat.routes.js';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-//Dar acceso al usuario a la carpeta public
-app.use(express.static(path.join( __dirname,"public")));
-
 const port = 8080;
 
-const httpServer =app.listen(port, () => {
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+//Acceso a la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Configuración hbs
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, './views'));
+
+// Routes
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartRouter);
+app.use('/api/chat', chatRouter);
+
+//Servidor websocket
+const httpServer = app.listen(port, () => {
   console.log('Servidor en línea!');
 });
 
 const socketServer = new Server(httpServer);
 
-//Configuración handlebars
-app.engine('.hbs', engine({extname: '.hbs'}));
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname,'./views'));
+const chat = [];
 
-//routes
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartRouter);
-
-//Conexión de socket
-socketServer.on("connection", (socket) => {
-  console.log("Cliente conectado", socket.id);
+socketServer.on("connection", (socket)=> {
+    console.log("Cliente conectado:", socket.id);
+    //Enviamos los mensajes cuando el usuario se conecta
+    socket.emit("chatHistory", chat)
+    //Recibimos mensaje de los usuarios
+    socket.on("chatMsg", (data)=> {
+        chat.push(data);
+        //Mandamos todos los mensajes de todos los usuarios (historial)
+        socketServer.emit("chatHistory", chat)
+    });
+    //Enviamos notificación de usuario conectado a todos menos el que se acaba de conectar (broadcast)
+    socket.on("userConnected", (user)=> {
+        socket.broadcast.emit("newUserConnected", `El usuario ${user} se acaba de conectar`);
+    })
 });
 
-//Conexión a la base de datos
+
+
+// Conexión a la base de datos
 connectDB();
-
-
