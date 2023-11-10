93bf64124ec9e6e33862a2fb6d8cf379987a9b66
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-11-10 17:02:59 -0300

    Se agrega configuración de session y mongoStore. Además rutas de sesiones

--- proyecto-backend/src/app.js
+++ proyecto-backend/src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import { json, urlencoded } from 'express';
 import path from 'path';
 import { __dirname } from './dirname.js';
-import { json, urlencoded } from 'express';
+import session  from "express-session";
+import MongoStore from "connect-mongo";
 import { engine } from 'express-handlebars';
-import { Server } from 'socket.io';
-import { connectDB } from './config/dbConnection.js';
-import { chatModel } from './dao/models/chat.model.js';
 import { productsRouter } from './routes/products.routes.js';
 import { cartRouter } from './routes/cart.routes.js';
 import { chatRouter } from './routes/chat.routes.js';
+import { sessionsRouter } from './routes/sessions.routes.js';
+import { Server } from 'socket.io';
+import { connectDB } from './config/dbConnection.js';
 
 const app = express();
 const port = 8080;
@@ -19,6 +21,19 @@ app.use(urlencoded({ extended: true }));
 //Acceso a la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configuración session y mongoStore
+app.use(session({
+    store: MongoStore.create({
+                mongoUrl: "mongodb+srv://franbourquin89:Umee4PfDvSsLVEle@francluster.0fwaxum.mongodb.net/e-commerce?retryWrites=true&w=majority",
+                ttl: 60
+    }),
+
+    secret: "e-commerceCoder2023",
+    resave: true,
+    saveUninitialized: true
+
+}))
+
 //Configuración hbs
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
@@ -28,6 +43,7 @@ app.set('views', path.join(__dirname, './views'));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/chat', chatRouter);
+app.use("/", sessionsRouter);
 
 //Servidor websocket
 const httpServer = app.listen(port, () => {
