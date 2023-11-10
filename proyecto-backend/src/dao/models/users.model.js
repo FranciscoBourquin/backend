8d1b13f9f2d371cce113421e89fec8d5b8019408
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-11-10 17:00:37 -0300

    Se corrige error de tipeo

--- proyecto-backend/src/dao/models/users.model.js
+++ proyecto-backend/src/dao/models/users.model.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const usersColl = "users";
 const usersSchema = new mongoose.Schema({
-    name: {tyepe: String, required: true},
+    name: {type: String, required: true},
     last_name: String,
     email: {required: true, unique:true, type: String},
     password: {required: true, unique: true, type:String },
     role: {type:String, enum: ["user", "admin"], default: "user"}
 });
 
-export const usersModels = mongoose.model(usersColl, usersSchema);
+export const usersModel = mongoose.model(usersColl, usersSchema);
