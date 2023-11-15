2429b53cb94b5588110608f158df7c08e46bd8ec
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-11-15 13:20:09 -0300

    Validación del login y mensaje de registro exitoso

--- proyecto-backend/src/routes/sessions.routes.js
+++ proyecto-backend/src/routes/sessions.routes.js
@@ -3,20 +3,56 @@ import { signupModel } from "../dao/models/signup.model.js";
 
 export const sessionsRouter = Router();
 
-sessionsRouter.get("/", (req, res)=> {
-    res.render("login");
+//Vista de login y mensaje de registro exitoso si se crea una cuenta
+sessionsRouter.get("/", async(req, res)=> {
+  const userInfo = req.body;
+  const registeredEmail = await signupModel.findOne({email: req.session.email});
+
+  if (registeredEmail!== null) {
+    res.render("login", {message: "Registro exitoso"});
+  }
+  else {
+    res.render("login")
+  }
+
 })
 
+//Validación del login
 sessionsRouter.post("/", async(req, res)=> {
+  const loginInfo = req.body;
+  const existingEmail = await signupModel.findOne({email:loginInfo.email});
+  const existingPassword = await signupModel.findOne( { password: loginInfo.password});
 
+  try {
+    if (existingEmail === null && existingPassword === null) {
+        res.render("login", {emailError: "El correo ingresado es incorrecto", passwordError: "La contraseña ingresada es incorrecta"})
+    }
+
+    else if (existingEmail === null && existingPassword !== null){
+      res.render("login", {emailError: "El correo ingresado es incorrecto"});
+    }
+
+    else if (existingEmail !== null && existingPassword === null) {
+      res.render("login", {passwordError: "La contraseña ingresada es incorrecta"})
+    }
+
+    else {
+      res.redirect("/api/products");
+    }
+
+  } catch (error) {
+      res.render("login", error.message)
+  }
 })
 
+//Vista del signup
 sessionsRouter.get("/signup", (req, res) => {
 
 
     res.render("signup");
   });
 
+  //Validación del egistro
   sessionsRouter.post("/signup", async(req, res)=> {
     const signupInfo = req.body;
 
@@ -26,7 +62,8 @@ try {
 
   if (existingEmail === null && existingPassword === null) {
     const newUser = await signupModel.create(signupInfo);
-    res.render("login", { message: "Usuario registrado exitosamente!" });
+    req. session.email = signupInfo.email;
+    res.redirect("/");
   } else {
     if (existingEmail !== null) {
       res.render("signup", { emailError: "El correo ingresado ya existe" });
