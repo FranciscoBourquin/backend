31c61ea3b6776918eee8287005354011c23c9bf5
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-11-28 20:02:40 -0300

    Se modifica logica de rutas usando passport

--- proyecto-backend/src/routes/sessions.routes.js
+++ proyecto-backend/src/routes/sessions.routes.js
@@ -1,57 +1,28 @@
 import { Router } from "express";
-import { signupModel } from "../dao/models/signup.model.js";
+import passport from "passport";
 
 export const sessionsRouter = Router();
 
 //Vista de login y mensaje de registro exitoso si se crea una cuenta
 sessionsRouter.get("/", async(req, res)=> {
-  const userInfo = req.body;
-  const registeredEmail = await signupModel.findOne({email: req.session.email});
-  if (req.session.login === false) {
-    req.session.destroy(
-      err => {
-        if (err) {
-          req.session.error = err;
-          res.redirect("/profile");
-        }
-        else {
-          res.render("login", {message: "Cierre de sesión exitoso"})
-        }
-      }
-    )}
-
-  else if (registeredEmail!== null) {
-    res.render("login", {message: "Registro exitoso"});
-  }
-  else {
-    res.render("login")
-  }}
-)
+  res.render("login")
+});
 
 //Validación del login
-sessionsRouter.post("/", async(req, res)=> {
-  const loginInfo = req.body;
-  const existingEmail = await signupModel.findOne({email:loginInfo.email});
-  const existingPassword = await signupModel.findOne( { password: loginInfo.password});
+sessionsRouter.post("/", passport.authenticate("loginLocalStrategy", {
+  failureRedirect: "/login/error"
+}), (req, res) => {
+  req.session.email = req.user.email;
+  req.session.name = req.user.name;
 
-  try {
+  // Redirige a la ruta de productos
+  res.redirect("/api/products");
+});
 
-        if (existingEmail === null && existingPassword === null) {
-        res.render("login", {emailError: "El correo ingresado es incorrecto", passwordError: "La contraseña ingresada es incorrecta"})
-    }
 
-    else if (existingEmail !== null && existingPassword === null) {
-      res.render("login", {passwordError: "La contraseña ingresada es incorrecta"})
-    }
 
-    else {
-      req.session.login = true
-      res.redirect("/api/products");
-    }
-
-  } catch (error) {
-      res.render("login", error.message)
-  }
+sessionsRouter.get("/login/error", (req, res)=> {
+  res.render("login", {message: "Error en el inicio de sesión"})
 })
 
 //Vista del signup
@@ -62,57 +33,28 @@ sessionsRouter.get("/signup", (req, res) => {
   });
 
   //Validación del registro
-  sessionsRouter.post("/signup", async(req, res)=> {
-    const signupInfo = req.body;
+  sessionsRouter.post("/signup",passport.authenticate("signupLocalStrategy", {
+    failureRedirect: "/signup/error",
+  }), async(req, res)=> {
 
-try {
-  const existingEmail = await signupModel.findOne({ email: signupInfo.email });
-  const existingPassword = await signupModel.findOne({ password: signupInfo.password });
-
-  if (existingEmail === null && existingPassword === null) {
-
-    const newUser = await signupModel.create(signupInfo);
-    req.session.name = signupInfo.name;
-    req.session.last_name = signupInfo.last_name;
-    req. session.email = signupInfo.email;
-    req.session.password = signupInfo.password;
-    res.redirect("/");
-  }
-
-  else {
-
-    if (existingEmail !== null) {
-      res.render("signup", { emailError: "El correo ingresado ya existe" });
-    }
-
-    else if (existingPassword !== null) {
-      res.render("signup", { passwordError: "La contraseña ingresada ya existe" });
-    }
-  }
-
-} catch (error) {
-
-  res.render("signup", { error: `Error al procesar la solicitud: ${error.message}` });
-}
-
+    res.render("login", {message: "Registro exitoso"});
+    // 1:41:49
   });
 
-
-sessionsRouter.get("/profile", async(req, res)=> {
-  const name = req.session.name;
-  const last_name = req.session.last_name;
-  const email = req.session.email;
-  const existingUser = await signupModel.findOne({email: email});
-  if (email) {
-
-    res.render("profile", {name, last_name, email})
-
-  }
-  else{
-    res.render("profile", {message: "Debes iniciar sesión para ver tu perfil"})
-  }
-
+sessionsRouter.get("signup/error", (req, res)=>{
+  res.render("signup", {message: "Error en el registro"});
 })
+
+// Ruta de perfil con autenticación
+sessionsRouter.get("/profile", (req, res) => {
+  if (req.isAuthenticated()) {
+      const { name, last_name, email } = req.user;
+      res.render("profile", { name, last_name, email });
+  } else {
+      res.render("profile", { message: "Debes iniciar sesión para ver tu perfil" });
+  }
+});
+
 
 sessionsRouter.get("/logout", (req, res)=> {
  if (req.session.error) {
