133e9289db2a29a1e5ed48228ea4e4482a1b2935
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-12-07 12:45:31 -0300

    Se agrega estrategia de inicio de sesión con GitHub

--- proyecto-backend/src/config/passport.config.js
+++ proyecto-backend/src/config/passport.config.js
@@ -2,9 +2,14 @@ import passport from "passport";
 import localStrategy from "passport-local";
 import { createHash, isValidPassword } from "../dirname.js";
 import { signupModel } from "../dao/models/signup.model.js";
+import { config } from "./config.js";
+import GithubStrategy from "passport-github2"
+
 
 export const initializePassport = ()=> {
     //middleware de passport: nombre de la estrategia + modulo de la estrategia (new localStrategy)
+
+    //Estrategia de signup
     passport.use("signupLocalStrategy", new localStrategy(
         {
             passReqToCallback: true,
@@ -35,7 +40,37 @@ export const initializePassport = ()=> {
         }
     ));
 
+    //Estrategia de signup con github
+    passport.use("githubSignupStrategy", new GithubStrategy(
+        {
+            clientID: config.github.clientId,
+            clientSecret: config.github.clientSecret,
+            callbackUrl: `http://localhost:8080${config.github.callbackUrl}`,
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                const user = await signupModel.findOne({email: profile.username});
+                if (user) {
+                    return done(null, user)
+                }
+                console.log("profile:", profile);
 
+                const newUser = {
+                    name: profile.displayName,
+                    email: profile.username,
+                    password: createHash(profile.id)
+                };
+                const createdUser = await signupModel.create(newUser)
+                console.log(newUser);
+                return done(null, createdUser);
+                }
+                  catch (error) {
+                    return done(error.message);
+            }
+        }
+    ));
+
+    //Estrategia de loign
     passport.use("loginLocalStrategy", new localStrategy(
         {
             usernameField: "email"
@@ -73,4 +108,4 @@ export const initializePassport = ()=> {
     });
 
 
-//2:02:01
+//1:02:01
