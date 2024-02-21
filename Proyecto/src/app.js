8fd37c9de4ae8d6e02a71abf4427bf24525c3e56
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-02-20 22:59:34 -0300

    Se importa manager y se crea ruta products

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -1,7 +1,11 @@
-import express from "express"
+import express from "express";
+import {ProductManager} from "./productManager.js";
+
+const path = "./products.json";
+const manager = new ProductManager(path);
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.listen(port, ()=> console.log(`Servidor escuchando en el puerto ${port}`));
 
@@ -9,3 +13,15 @@ app.get("/", (req, res)=> {
 
     res.send("<h1>Página de inicio</h1>")
 });
+
+app.get("/products", async (req,res) => {
+    const limit = req.query;
+    if (limit) {
+        const limitedProducts = await manager.getProducts().slice(0, limit)
+        res.send(limitedProducts)
+
+    } else {
+        const products = await manager.getProducts();
+        res.send(products)
+    }
+})
