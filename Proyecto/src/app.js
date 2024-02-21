abb2e8e003a665e46f7774e2e16339d510c4dd08
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-02-21 12:19:21 -0300

    Se corrige la ruta del json y se agrega el json.stringify a products para mejor visualización

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -1,27 +1,26 @@
 import express from "express";
-import {ProductManager} from "./productManager.js";
+import { ProductManager } from "./productManager.js";
 
-const path = "./products.json";
-const manager = new ProductManager(path);
-
+const manager = new ProductManager("./src/products.json");
 const app = express();
 const port = 8080;
 
-app.listen(port, ()=> console.log(`Servidor escuchando en el puerto ${port}`));
+app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`));
 
-app.get("/", (req, res)=> {
-
-    res.send("<h1>Página de inicio</h1>")
+app.get("/", (req, res) => {
+    res.send("<h1>Página de inicio</h1>");
 });
 
-app.get("/products", async (req,res) => {
-    const limit = req.query;
+app.get("/products", async (req, res) => {
+    const limit = req.query.limit;
     if (limit) {
-        const limitedProducts = await manager.getProducts().slice(0, limit)
-        res.send(limitedProducts)
-
+        const products = await manager.getProducts();
+        const limitedProducts = products.slice(0, limit);
+        const jsonOutput = JSON.stringify(limitedProducts, null, 2);
+        res.type("json").send(jsonOutput);
     } else {
         const products = await manager.getProducts();
-        res.send(products)
+        const jsonOutput = JSON.stringify(products, null, 2);
+        res.type("json").send(jsonOutput);
     }
-})
+});
