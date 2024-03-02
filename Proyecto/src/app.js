7dbd27e7dad81162e18500a7ad5c24fb5783270e
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-02 01:16:21 -0300

    Conexión con routers de productos y carritos

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -1,41 +1,24 @@
 import express from "express";
-import { ProductManager } from "./productManager.js";
+import { productsRouter } from "./routes/products.routes.js";
+import { cartsRouter } from "./routes/carts.routes.js";
 
-const manager = new ProductManager("./src/products.json");
 const app = express();
 const port = 8080;
 
-app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`));
+//Para poder procesar respuestas en fomato JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.get("/", (req, res) => {
-    res.send("<h1>Página de inicio</h1>");
+app.listen(port, ()=> console.log(`Servidor escuchando en el puerto ${port}`));
+
+//Rutas
+app.get("/", (req, res)=> {
+
+    res.send("<h1>Página de inicio</h1>")
 });
 
-//Obtenemos todos los productos o la cantidad establecida en limit
-app.get("/products", async (req, res) => {
-    const limit = req.query.limit;
-    if (limit) {
-        const products = await manager.getProducts();
-        const limitedProducts = products.slice(0, limit);
-        const jsonOutput = JSON.stringify(limitedProducts, null, 2);
-        res.type("json").send(jsonOutput);
-    } else {
-        const products = await manager.getProducts();
-        const productsJson = JSON.stringify(products, null, 2);
-        res.type("json").send(productsJson);
-    }
-});
+//Conexión con router de productos
+app.use("/api/products", productsRouter);
 
-//Obtenemos un producto por su id
-app.get("/products/:pid", async(req,res)=> {
-    const pid = parseInt(req.params.pid);
-    if (pid) {
-        const product = await manager.getProductById(pid);
-        const productJson = JSON.stringify(product, null, 2);
-        res.type("json").send(productJson)
-
-    } else {
-        res.send(`El producto con ID ${pid} no existe`)
-    }
-});
-
+//Conexion con router de carritos
+app.use("/api/carts", cartsRouter);
