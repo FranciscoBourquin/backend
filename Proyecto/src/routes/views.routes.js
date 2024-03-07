50a9b6ac16e52b8bac2607ad921fd8a700fd302c
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-06 21:54:52 -0300

    se crea router de vistas

---
+++ Proyecto/src/routes/views.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { ProductManager } from "../productManager.js";
+
+const roleAuth = (req, res, next) => {
+    const role = req.query.role;
+    if (role === "admin") {
+        next();
+    } else {
+        res.send("No tienes permisos para acceder a esta pagina");
+    }
+}
+
+const manager = new ProductManager("./src/products.json")
+export const viewsRouter = Router();
+
+viewsRouter.get("/", async(req, res)=> {
+    try {
+        const limit = req.query.limit;
+        let products = await manager.getProducts();
+        if (limit) {
+            products = products.slice(0, limit);
+        }
+        res.render("home", {products});
+    } catch (error) {
+        console.error(`Error al obtener productos: ${error.message}`);
+        res.render("home", {errorMessage: error.message});
+    }
+
+});
+
+viewsRouter.get("/realtimeproducts",roleAuth, async(req, res)=> {
+    try {
+        let products = await manager.getProducts();
+        res.render("products", {products})
+    } catch (error) {
+        res.render("products", {errorMessage: error.message})
+    }
+});
