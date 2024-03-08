955697a83a5a528e3e8e723c7d6b5164a311aa31
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-08 02:18:11 -0300

    Creacion del servidor con socket, y envío de productos al cliente

--- Proyecto/src/app.js
+++ Proyecto/src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {Server} from "socket.io";
 import {engine} from "express-handlebars";
-// import {Server} from "socket.io;"
 import { viewsRouter } from "./routes/views.routes.js";
 import { productsRouter } from "./routes/products.routes.js";
 import { cartsRouter } from "./routes/carts.routes.js";
+import { ProductManager } from "./productManager.js";
 
 const app = express();
 const port = 8080;
@@ -11,8 +12,11 @@ const port = 8080;
 //Para poder procesar respuestas en fomato JSON
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("./src/public"));
 
-app.listen(port, ()=> console.log(`Servidor escuchando en el puerto ${port}`));
+const hhttpServer = app.listen(port, ()=> console.log(`Servidor escuchando en el puerto ${port}`));
+
+const socketServer = new Server(hhttpServer);
 
 //Configuración hbs
 app.engine('.hbs', engine({extname: '.hbs'}));
@@ -30,4 +34,11 @@ app.use("/api/products", productsRouter);
 //Conexion con router de carritos
 app.use("/api/carts", cartsRouter);
 
+const manager = new ProductManager("./src/products.json");
+
+socketServer.on("connection", async(socket)=> {
+    console.log(`Cliente conectado con ID: ${socket.id}`);
+    const products = await manager.getProducts();
+    socket.emit("productsArray", {products});
+})
 
