47ea9fb2575f386dacdba3504ac690a4b74a56c3
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-08 17:22:31 -0300

    se reciben productos, se recorren y se muestran en plantilla. Se captura elemento ul

--- Proyecto/src/public/js/realTimeProducts.js
+++ Proyecto/src/public/js/realTimeProducts.js
@@ -1,7 +1,19 @@
 console.log("JS de real time products conectado");
 const socketClient = io();
+let productUl = document.getElementById("productList");
 
 socketClient.on("productsArray", (productList)=> {
     console.log(productList);
+    let liElements = ""
 
+    productList.forEach(product => {
+        liElements+=
+        `<li>nombre: ${product.title}</li>
+        <li>precio: ${product.price}</li>
+        <li>stock: ${product.stock}</li>
+        <hr />`
+
+    })
+    console.log(liElements);
+    productUl.innerHTML = liElements
 })
