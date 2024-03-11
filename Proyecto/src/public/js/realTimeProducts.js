02b4ea8bb9b17d46f110231742b9417500a92e84
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-11 20:39:07 -0300

    logica para recibir info del formulario para crear producto

--- Proyecto/src/public/js/realTimeProducts.js
+++ Proyecto/src/public/js/realTimeProducts.js
@@ -1,7 +1,24 @@
 console.log("JS de real time products conectado");
-const socketClient = io();
-let productUl = document.getElementById("productList");
 
+const socketClient = io();
+
+let productUl = document.getElementById("productList");
+const createProductForm = document.getElementById("createProductForm");
+const deleteProductForm = document.getElementById("deleteProductForm");
+
+//Enviamos info del formulario para crear producto
+createProductForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const formData = new FormData(createProductForm);
+    const jsonData = {};
+    for (const [key, value] of formData.entries()) {
+        jsonData[key] = value;
+    }
+    socketClient.emit("addProduct", jsonData);
+    createProductForm.reset();
+});
+
+//Recibimos productos
 socketClient.on("productsArray", (productList)=> {
     console.log(productList);
     let liElements = ""
@@ -9,6 +26,7 @@ socketClient.on("productsArray", (productList)=> {
     productList.forEach(product => {
         liElements+=
         `<li>nombre: ${product.title}</li>
+        <img src="${product.thumbnail}"/>
         <li>precio: ${product.price}</li>
         <li>stock: ${product.stock}</li>
         <hr />`
@@ -17,3 +35,5 @@ socketClient.on("productsArray", (productList)=> {
     console.log(liElements);
     productUl.innerHTML = liElements
 })
+
+//after websocket 1:10:00
