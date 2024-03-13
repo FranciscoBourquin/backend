7fd69517d81933eb7774f4eacc3621779a1d5b4d
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-03-13 20:43:07 -0300

    se agregan console.logs para debugging del lado del cliente

--- Proyecto/src/public/js/realTimeProducts.js
+++ Proyecto/src/public/js/realTimeProducts.js
@@ -1,11 +1,14 @@
 const socketClient = io();
-
+console.log("js para rtp conectado");
 let productUl = document.getElementById("productList");
 const createProductForm = document.getElementById("createProductForm");
 const deleteProductForm = document.getElementById("deleteProductForm");
 
 // Función para eliminar producto
-const deleteProduct = productId => socketClient.emit("deleteProduct", productId);
+const deleteProduct = productId =>{
+    console.log(`Datos enviados desde el cliente... se eliminará el producto con ID ${productId}`);
+     return socketClient.emit("deleteProduct", productId)
+};
 
 // Enviamos info del formulario para crear producto
 createProductForm.addEventListener("submit", (e)=> {
@@ -29,11 +32,17 @@ socketClient.on("productsArray", (productList)=> {
         <img src="${product.thumbnail}"/>
         <li>precio: ${product.price}</li>
         <li>stock: ${product.stock}</li>
-        <button onclick="deleteProduct(${product.id})">Eliminar Producto</button>
+        <button id= "delete-btn-${product.id}" data-product-id="${product.id}">Eliminar Producto</button>
         <hr />`;
     });
 
     productUl.innerHTML = liElements;
+
+    productList.forEach(product=> {
+        document.getElementById(`delete-btn-${product.id}`).addEventListener("click", ()=> {
+            deleteProduct(product.id)
+        })
+    })
 });
 
 console.log(deleteProduct);
